5ac01ca57eb33007c29edf4b5680ff00d5d0e65e
Pedro di Luca <137642016+Ex-Machina-Unifei-Oficial@users.noreply.github.com>
2024-08-04 12:09:46 -0300

    removed unused code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,29 +2,6 @@
 // import { Drawer } from "expo-router/drawer";
 // import { StatusBar, View } from "react-native";
 
-// export default function RootLayout() {
-//   return (
-//     <>
-//       <Stack
-//         screenOptions={{
-//           headerShown: true,
-//           headerStyle: { backgroundColor: "#def" },
-//           // statusBarColor: "#def",
-//           // statusBarStyle: "dark",
-//         }}
-//       >
-//         <Stack.Screen name="index" options={{ title: "Home" }} />
-//       </Stack>
-//       {/* <StatusBar
-//         animated={false}
-//         backgroundColor="#def"
-//         barStyle={"dark-content"}
-//         hidden={false}
-//       /> */}
-//     </>
-//   );
-// }
-
 // import { TabsNavigation } from "@/roteamento/tabs";
 // export default TabsNavigation;
 
